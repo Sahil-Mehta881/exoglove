5dabd033d8f7a766522862f296bdf49bf453021c
Sahil Mehta <mehtasahil881@berkeley.edu>
2022-04-17 15:57:06 -0700

    skeleton code and notes

--- exoglove.ino
+++ exoglove.ino
@@ -6,4 +6,40 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+  /*
+
+
+   Notes:
+   - default deflated 
+   - automatically inflates
+   - both valves open by default
+   
+   Pseudocode:
+
+   main:
+   voltage_to_send = sensor_activate()
+    
+    
+   else 
+    deflate()
+    
+
+   sensor_activate() {
+     check if voltage is within a range of the previous value
+     store current voltage for next cycle
+     return specific voltage value to send to valve 
+     (determines amount to inflate finger)
+   }
+   
+
+   inflate() {
+     close exit valve
+   }
+
+   deflate() {
+     open exit valave
+   }
+     
+   */
+
 }
